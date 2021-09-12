ad4ec8bb0bd0a01e0f3048c16d7c2ef252bccd17
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-12 14:40:40 +0700

    add Title

---
+++ web-portfolio/src/Components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function Title(){
+    return(
+        <TitleStyled></TitleStyled>
+    );
+}
+
+const TitleStyled = styled.div`
+
+`;
+export default Title;
