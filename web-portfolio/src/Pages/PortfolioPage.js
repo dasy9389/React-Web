62fca8fcb9a3485c5f0d3d15db43b2b722c5455f
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-13 11:43:47 +0700

    portfolios

--- web-portfolio/src/Pages/PortfolioPage.js
+++ web-portfolio/src/Pages/PortfolioPage.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { InnerLayout, MainLayout } from "../styles/Layouts";
+import Menu from '../Components/Menu';
+import portfolios from "../data/portfolios";
+import Title from '../Components/Title';
+import Button from "../Components/Button";
+
+const allButtons = ['All', ...new Set(portfolios.map(item=>item.category))];
 
 function PortfolioPage(){
+    const [menuItem, setMenuItems] = useState(portfolios);
+    const [button, setButtons] = useState(allButtons);
+
+    const filter = (button) => {
+        if(button === 'All'){
+            setMenuItems(portfolios);
+            return;
+        }
+
+        const filterData = portfolios.filter(item => item.category === button);
+        setMenuItems(filterData);
+    }
+
     return(
-        <PortfolioPageStyled></PortfolioPageStyled>
+        <MainLayout>
+            <PortfolioPageStyled>
+                <Title title={'Portofios'} span={'portfolios'} />
+                <InnerLayout>
+                    <Button filter={filter} button={button} />
+                    <Menu menuItem={menuItem} />
+                </InnerLayout>
+            </PortfolioPageStyled>
+        </MainLayout>
     );
 }
 
