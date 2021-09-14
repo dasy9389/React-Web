c0ee97e0c1efc6f55c34d22ffaf1380be0ca5821
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-14 09:29:28 +0700

    add animationspage

--- web-portfolio/src/index.js
+++ web-portfolio/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
